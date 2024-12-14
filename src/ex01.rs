361e1cc275c97c8bff1b10b7640e443ba1191870
Léo <leogaudinoff@gmail.com>
2024-12-14 16:56:50 +0100

    fix: shorter way

--- src/ex01.rs
+++ src/ex01.rs
@@ -15,25 +15,24 @@ K: std::fmt::Display
     if v.len() != a.len() {
         panic!("");
     }
-    let mut scaled = v.iter().map(|x| x.clone()).collect::<Vec<Vector<K>>>();
-    for i in 0..a.len() {
-        scaled[i].scl(a[i]);
-    }
 
-    let mut accum = Vector::from(scaled[0].clone());
-    for i in 1..scaled.len() {
-        accum.add(&scaled[i]);
+    let mut accum = v[0].clone();
+    accum.scl(a[0]);
+    for i in 1..v.len() {
+        let mut current = v[i].clone();
+        current.scl(a[i]);
+        accum.add(&current);
     }
 
     return accum;
 }
 
 pub fn test_01() {
-    let e1 = Vector::from_1d(vec![1., 0., 0.]);
-    let e2 = Vector::from_1d(vec![0., 1., 0.]);
-    let e3 = Vector::from_1d(vec![0., 0., 1.]);
-    let v1 = Vector::from_1d(vec![1., 2., 3.]);
-    let v2 = Vector::from_1d(vec![0., 10., -100.]);
+    let e1 = Vector::from(vec![1., 0., 0.]);
+    let e2 = Vector::from(vec![0., 1., 0.]);
+    let e3 = Vector::from(vec![0., 0., 1.]);
+    let v1 = Vector::from(vec![1., 2., 3.]);
+    let v2 = Vector::from(vec![0., 10., -100.]);
     println!("{}\n", linear_combination(&[e1, e2, e3], &[10., -2., 0.5]));
     // [10.]
     // [-2.]
